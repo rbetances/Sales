fd2ab28985b1d67c87c8be7bdcf3c8c547d3b0fe
rbetances <ramonronaldobetances@hotmail.com>
2020-04-03 14:33:24 -0400

    Creacion del proyecto

---
+++ Sales/Sales/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Sales.Models;
+
+namespace Sales.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
