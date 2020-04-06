74ab2ade6603638a4409661574341da12ed5113e
rbetances <ramonronaldobetances@hotmail.com>
2020-04-06 17:00:52 -0400

    Start Project

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿using Sales.Domain.Models;
+
+namespace Sales.Backend.Models
+{
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data.Entity;
+
+namespace Sales.Domain.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
