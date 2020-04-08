eef3b1181f78d08962b0fab3c9c812838323930c
rbetances <ramonronaldobetances@hotmail.com>
2020-04-07 20:25:03 -0400

    Add ApiService

---
+++ Sales.Common/Models/Response.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Sales.Common.Models
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Sales.Common.Models;
+
+namespace Sales.Services
+{
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}/{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response { IsSuccess = false, Message = answer };
+                }
+
+                object objetoSerializado = JsonConvert.DeserializeObject<List<T>>(answer);
+
+                return new Response { IsSuccess = true, Result = objetoSerializado };
+
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
