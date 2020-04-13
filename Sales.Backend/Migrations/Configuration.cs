1cedeb8577986008dc6d0c45e3f26f6163e91554
rbetances <ramonronaldobetances@hotmail.com>
2020-04-12 23:03:14 -0400

    Correccion en la migracion

--- Sales.Backend/Migrations/Configuration.cs
+++ Sales.Backend/Migrations/Configuration.cs
@@ -1,13 +1,15 @@
 namespace Sales.Backend.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Sales.Backend.Models.LocalDataContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Sales.Backend.Models.LocalDataContext context)
