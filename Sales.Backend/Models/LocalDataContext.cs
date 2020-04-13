5ee637a71298c18bb8195c98353bc6b1d0a7ece6
rbetances <ramonronaldobetances@hotmail.com>
2020-04-12 22:50:41 -0400

    Add Images Backend

--- Sales.Backend/Models/LocalDataContext.cs
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -4,6 +4,5 @@ namespace Sales.Backend.Models
 {
     public class LocalDataContext : DataContext
     {
-        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
     }
 }
